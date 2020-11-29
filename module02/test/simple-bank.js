3964142b9f078e34b95e7d3d0ff7493428b67f01
vgorin <vgrn.at@gmail.com>
2020-11-29 21:38:12 +0200

    module 2 test fix: open an account

--- module02/test/simple-bank.js
+++ module02/test/simple-bank.js
@@ -13,6 +13,35 @@ contract("SimpleBank.sol", (accounts) => {
 		assert(await bank.exists({from: accounts[1]}), "account 1 doesn't exist after it was created");
 
 		// verify account 1 balance is zero
-		assert.equal(0, await bank.balance({from: accounts[1]}), "account 1 balance is not zero after it was just created")
+		assert.equal(0, await bank.balance({from: accounts[1]}), "account 1 balance is not zero after it was just created");
+
+		// verify it is impossible to create same account again
+		await expectThrow(bank.open({from: accounts[1]}), "able to create account 1 again");
 	});
 });
+
+
+// auxiliary function to ensure function throws
+// taken from OpenZeppelin expectThrow script
+async function expectThrow(promise, msg) {
+	try {
+		await promise;
+	}
+	catch (error) {
+		// TODO: Check jump destination to distinguish between a throw
+		//       and an actual invalid jump.
+		const invalidOpcode = error.message.search('invalid opcode') >= 0;
+		// TODO: When we contract A calls contract B, and B throws, instead
+		//       of an 'invalid jump', we get an 'out of gas' error. How do
+		//       we distinguish this from an actual out of gas event? (The
+		//       ganache log actually show an 'invalid jump' event.)
+		const outOfGas = error.message.search('out of gas') >= 0;
+		const revert = error.message.search('revert') >= 0;
+		assert(
+			invalidOpcode || outOfGas || revert,
+			'Expected throw, got \'' + error + '\' instead',
+		);
+		return;
+	}
+	assert.fail(msg);
+}

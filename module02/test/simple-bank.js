8abcd8611bacc33612c3e35ada53ec261216dac3
vgorin <vgrn.at@gmail.com>
2020-11-29 22:02:05 +0200

    module 2 test: deposit

--- module02/test/simple-bank.js
+++ module02/test/simple-bank.js
@@ -10,13 +10,33 @@ contract("SimpleBank.sol", (accounts) => {
 		await bank.open({from: accounts[1]});
 
 		// verify account 1 exists
-		assert(await bank.exists({from: accounts[1]}), "account 1 doesn't exist after it was created");
+		assert(await bank.exists({from: accounts[1]}), "bank account doesn't exist after it was created");
 
 		// verify account 1 balance is zero
-		assert.equal(0, await bank.balance({from: accounts[1]}), "account 1 balance is not zero after it was just created");
+		assert.equal(0, await bank.balance({from: accounts[1]}), "bank account balance is not zero after it was just created");
 
 		// verify it is impossible to create same account again
-		await expectThrow(bank.open({from: accounts[1]}), "able to create account 1 again");
+		await expectThrow(bank.open({from: accounts[1]}), "able to create same bank account again");
+	});
+
+	it("deposit", async() => {
+		// deploy SimpleBank smart contract into network
+		const bank = await SimpleBankContract.new();
+
+		// verify it is impossible to deposit into non-existing account
+		await expectThrow(bank.deposit({from: accounts[1], value: 1}), "able to deposit 1 wei into non-existent account");
+
+		// open account 1 – for accounts[1]
+		await bank.open({from: accounts[1]});
+
+		// deposit 1 wei into account 1
+		await bank.deposit({from: accounts[1], value: 1});
+
+		// verify 1 wei deposited successfully
+		assert.equal(1, await bank.balance({from: accounts[1]}), "bank account balance is not 1 wei after depositing 1 wei");
+
+		// verify it is impossible to deposit zero value
+		await expectThrow(bank.deposit({from: accounts[1]}), "able to deposit zero value");
 	});
 });
 
